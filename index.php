2a9219810acef2758625e48f16a526e7f70d6f09
Alexey <Solovey_alexey3@mail.ru>
2016-02-27 10:02:20 +0200

    start

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>La provance</title>
+	<link href="css/style.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+
+
+
+<!--footer-->
+<div class="container-fluid i-footer-wrapper">
+    <div class="container main_container">
+        <div class="col-xs-12 col-md-3 col-lg-4 i_footer_block">
+            <a href="#"><img src="img/facebook_icon.png" class="i_social_icons"></a>
+            <a href="#"><img src="img/google_icon.png" class="i_social_icons"></a>
+            <a href="#"><img src="img/instagram_icon.png" class="i_social_icons"></a>
+            <a href="#"><img src="img/pinterest_icon.png" class="i_social_icons"></a>
+            <a href="#"><img src="img/twitter_icon.png" class="i_social_icons"></a>
+        </div>
+        <div class="col-xs-9 col-lg-8 hidden-xs hidden-sm">
+            <div class="col-xs-3 col-lg-3 i_footer_block">
+                <ul>
+                    <li><a href="#">من نحن</a></li>
+                    <li><a href="#">اعلن معنا</a></li>
+                    <li><a href="#">اتصل بنا</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-3 col-lg-3 i_footer_block">
+                <ul>
+                    <li><a href="#">أكسسوارات وفنون</a></li>
+                    <li><a href="#">أصدقاء البيئة</a></li>
+                    <li><a href="#">الفنون المعمارية</a></li>
+                    <li><a href="#">مصممون ومهنيون</a></li>
+                    <li><a href="#">متجر caZain</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-3 col-lg-3 i_footer_block">
+                <ul>
+                    <li><a href="#">الحديقة والتراس</a></li>
+                    <li><a href="#">الجلوس والسفرة</a></li>
+                    <li><a href="#">الحمامات</a></li>
+                    <li><a href="#">المطابخ</a></li>
+                    <li><a href="#">المكاتب</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-3 col-lg-3 i_footer_block i_footer_block_last">
+                <ul>
+                    <li><a href="#"> صور</a></li>
+                    <li><a href="#">فيديو caZain</a></li>
+                    <li><a href="#">نصائح caZain</a></li>
+                    <li><a href="#">الأمن والمنزل الذكي</a></li>
+                    <li><a href="#">الإنارة</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
