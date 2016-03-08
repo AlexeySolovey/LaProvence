8bfd85e47a8c1b57ee88ad277b2b97b5727602b1
Alexey <Solovey_alexey3@mail.ru>
2016-03-08 11:33:17 +0200

    end first page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>La provance</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link href="css/mobile.css" rel="stylesheet" media="only screen and (min-width:320px)" />
-	<link href="css/style.css" rel="stylesheet" media="screen">
+	<link href="css/style.css" rel="stylesheet" media="only screen and (min-width:1200px)">
 	<link href="css/hover.css" rel="stylesheet" media="screen">
 	<link href="css/fonts.css" rel="stylesheet" media="screen">
 	<link href="css/bootstrap.css" rel="stylesheet" media="screen">
@@ -12,210 +12,211 @@
 </head>
 <body>
 
-
-<header>
-	<div class="container">
-		<div class="row text-right login">
-			<a href="#" class="register">Регистрация </a>
-			<a href="#" class="enter">вход </a>
-			<button type="button" class="serch"></button>
-		</div>
-		<div class="row text-center">
-			<a href="#"> 
-				<img src="images/logo.png" class="logo" alt="">
-			</a>
-			<p class="disk-shope">
-				магазин домашнего текстиля
-			</p>
-		</div>
-	</div>
-</header>
-<div class="navbar">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
-					<span class="sr-only">Открыть навигацию</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-			</button>
-		</div>
-		<div class="collapse navbar-collapse" id="responsive-menu">
-			<ul class="nav navbar-nav">
-				<li><a href="#">Главная</a></li>
-				<li><a href="#">Каталог</a></li>
-				<li><a href="#">О льне</a></li>
-				<li><a href="#">Доставка/Оплата</a></li>
-				<li><a href="#">Новости</a></li>
-				<li><a href="#">Скидки</a></li>
-				<li><a href="#">Контакты</a></li>
-			</ul>
-			<div class="navbar-right">
-				<a href="#" class="enter">
-					<span class="basket">Корзина</span>
-					<span class="count">x1</span>
-					<span class="sum">1 500 грн</span>
-					<button type="button" class="bag"></button>
+<div class="wrap">
+	<header>
+		<div class="container">
+			<div class="row text-right login">
+				<a href="#" class="register">Регистрация </a>
+				<a href="#" class="enter">вход </a>
+				<button type="button" class="serch"></button>
+			</div>
+			<div class="row text-center">
+				<a href="#"> 
+					<img src="images/logo.png" class="logo" alt="">
 				</a>
+				<p class="disk-shope">
+					магазин домашнего текстиля
+				</p>
+			</div>
+		</div>
+	</header>
+	<div class="navbar">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+						<span class="sr-only">Открыть навигацию</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse" id="responsive-menu">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="catalog.html">Каталог</a></li>
+					<li><a href="#">О льне</a></li>
+					<li><a href="#">Доставка/Оплата</a></li>
+					<li><a href="#">Новости</a></li>
+					<li><a href="#">Скидки</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+				<div class="navbar-right">
+					<a href="#" class="enter">
+						<span class="basket">Корзина</span>
+						<span class="count">x1</span>
+						<span class="sum">1 500 грн</span>
+						<button type="button" class="bag"></button>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<div class="ellipses"></div>
-<!--start baner-->
-<div class="baner">
-	<div class="contaner">
-		<div class="row">
-			<div class="col-xs-4"></div>
-			<div class="col-xs-2">
-				<div class="text text-right">
-					НА ЛЬНЯНОЕ ПОСТЕЛЬНОЕ БЕЛЬЕ СКИДКА
-				</div>
-			</div>
-			<div class="col-xs-2">
-				<div class="per_sent">
-				50%
-				</div>
+	<div class="ellipses"></div>
+	<!--start baner-->
+	<div class="baner">
+		<div class="contaner">
+			<div class="row">
 				<div class="col-xs-4"></div>
-			</div>
-		</div>
-	</div>
-</div>
-<!--end baner-->
-<!--start big contant-->
-<div class="container">
-	<!--news-->
-	<div class="news">
-		<div class="row">
-			<div class="col-xs-12">
-				<h4 class="text-center">Новинки</h4>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+				<div class="col-xs-2">
+					<div class="text text-right">
+						НА ЛЬНЯНОЕ ПОСТЕЛЬНОЕ БЕЛЬЕ СКИДКА
+					</div>
 				</div>
-			</div>
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
-				</div>
-			</div>
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
-				</div>
-			</div>
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+				<div class="col-xs-2">
+					<div class="per_sent">
+					50%
+					</div>
+					<div class="col-xs-4"></div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!--end news-->
-	<!--start discount-->
-	<div class="discount">
-		<div class="row">
-			<div class="col-xs-12">
-				<h4 class="text-center">Скидки</h4>
+	<!--end baner-->
+	<!--start big contant-->
+	<div class="container">
+		<!--news-->
+		<div class="news">
+			<div class="row">
+				<div class="col-xs-12">
+					<h4 class="text-center">Новинки</h4>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
 			</div>
 		</div>
+		<!--end news-->
+		<!--start discount-->
+		<div class="discount">
+			<div class="row">
+				<div class="col-xs-12">
+					<h4 class="text-center">Скидки</h4>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<img src="images/goods1.png" class="img-responsive">
+					<div class="information">
+						<p class="describe">Покрывало в клеточку</p>
+						<p class="size">150x200 см</p>
+						<span class="past-price">35 000 грн</span>
+						<span class="now-price">30 000 грн</span>
+						<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--end discount-->
+	</div>
+	<!--end big content-->
+	<!--start Text block-->
+	<div class="container">
 		<div class="row">
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
-				</div>
-			</div>
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
-				</div>
-			</div>
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
-				</div>
-			</div>
-			<div class="col-xs-3">
-				<img src="images/goods1.png" class="img-responsive">
-				<div class="information">
-					<p class="describe">Покрывало в клеточку</p>
-					<p class="size">150x200 см</p>
-					<span class="past-price">35 000 грн</span>
-					<span class="now-price">30 000 грн</span>
-					<button type="button" class="hvr-outline-out">В КОРЗИНУ</button>
-				</div>
+			<div class="text">
+				<h2>Магазин домашнего текстиля «Ля Прованс»
+		Предлагает экологичные, натуральные и красивые
+		изделия для дома:</h2>
+				<p>Продукция Оршанского льнокомбината (Беларусь).</p>
+				<p>Разнообразное столовое и постельное белье, льняные махровые полотенца, рушники,
+				наборы для сауны и кухни, покрывала и пледы, сумки.</p>
+				<p>Льняные интерьерные игрушки ручной работы украсят дом и поднимут настроение.</p>
+				<p>Французское постельное белье "La Scala".</p>
+				<p>Материал сатин (хлопок 100%) обеспечит здоровый сон.</p>
+				<p>Мы предложим Вам товары высшего качества, которые наполнят жизнь гармонией и комфортом, создадут в доме праздник и будут прекрасным подарком к любому торжеству!</p>
+				<br/>
+				<p>Магазин домашнего текстиля "Ля Прованс"  г. Кривой Рог, пр. Мира 27</p>
+				<p>Заказ по телефону  067 971-22-35,  096 704-62-13.</p>
+				<p>e-mail: 0967046213@mail.ru</p>
+				<br/><br/>
+				<p>Синие, голубые, лиловые, порой розовые и белые цветки на длинных стеблях, ровно застилающие поля – фантастическую красоту нежнейших переливов цветущего льна можно увидеть только до полудня, под жарким солнцем цветки складываются или осыпаются. Лен цветет всего несколько дней, но то, что научились создавать из него люди, оказалось на редкость долговечным, надежным и прочным. Льняные ткани издавна применяются для изготовления нательного, постельного и столового белья.</p>
+				<p>Лен имеет удивительные свойства: экологически чистый, прохладный в жару и теплый в холод, является антисептиком, хорошо впитывает влагу и быстро высыхает. Поэтому льняные ткани создают комфортную и здоровую обстановку, а сон на льняном белье считается лучшим способом восстановить силы и душевное равновесие. Этому, конечно, есть научное объяснение: химически необработанный лен содержит такие ценные элементы, как кремний и селен, и во время сна организм восполняет их дефицит. Но, несомненно и другое - лен обладает доброй энергетикой, не поддающейся никакому объяснению.</p>
+				<p>Природные свойства льна прекрасно отвечают требованиям, необходимым  для интерьерного текстиля: они не деформируются, не теряют яркости и блеска. Льняные ткани более стойки к механическим  воздействиям и с каждой стиркой льняная ткань становится только мягче.</p>
+				<p>Лён - материал настолько древний, что уже один этот факт вызывает интерес и уважение. То, что проверено веками и тысячелетиями, достойно внимательного изучения.</p>
 			</div>
 		</div>
 	</div>
-	<!--end discount-->
-</div>
-<!--end big content-->
-<!--start Text block-->
-<div class="container">
-	<div class="row">
-		<div class="text">
-			<h2>Магазин домашнего текстиля «Ля Прованс»
-	Предлагает экологичные, натуральные и красивые
-	изделия для дома:</h2>
-			<p>Продукция Оршанского льнокомбината (Беларусь).</p>
-			<p>Разнообразное столовое и постельное белье, льняные махровые полотенца, рушники,
-			наборы для сауны и кухни, покрывала и пледы, сумки.</p>
-			<p>Льняные интерьерные игрушки ручной работы украсят дом и поднимут настроение.</p>
-			<p>Французское постельное белье "La Scala".</p>
-			<p>Материал сатин (хлопок 100%) обеспечит здоровый сон.</p>
-			<p>Мы предложим Вам товары высшего качества, которые наполнят жизнь гармонией и комфортом, создадут в доме праздник и будут прекрасным подарком к любому торжеству!</p>
-			<br/>
-			<p>Магазин домашнего текстиля "Ля Прованс"  г. Кривой Рог, пр. Мира 27</p>
-			<p>Заказ по телефону  067 971-22-35,  096 704-62-13.</p>
-			<p>e-mail: 0967046213@mail.ru</p>
-			<br/><br/>
-			<p>Синие, голубые, лиловые, порой розовые и белые цветки на длинных стеблях, ровно застилающие поля – фантастическую красоту нежнейших переливов цветущего льна можно увидеть только до полудня, под жарким солнцем цветки складываются или осыпаются. Лен цветет всего несколько дней, но то, что научились создавать из него люди, оказалось на редкость долговечным, надежным и прочным. Льняные ткани издавна применяются для изготовления нательного, постельного и столового белья.</p>
-			<p>Лен имеет удивительные свойства: экологически чистый, прохладный в жару и теплый в холод, является антисептиком, хорошо впитывает влагу и быстро высыхает. Поэтому льняные ткани создают комфортную и здоровую обстановку, а сон на льняном белье считается лучшим способом восстановить силы и душевное равновесие. Этому, конечно, есть научное объяснение: химически необработанный лен содержит такие ценные элементы, как кремний и селен, и во время сна организм восполняет их дефицит. Но, несомненно и другое - лен обладает доброй энергетикой, не поддающейся никакому объяснению.</p>
-			<p>Природные свойства льна прекрасно отвечают требованиям, необходимым  для интерьерного текстиля: они не деформируются, не теряют яркости и блеска. Льняные ткани более стойки к механическим  воздействиям и с каждой стиркой льняная ткань становится только мягче.</p>
-			<p>Лён - материал настолько древний, что уже один этот факт вызывает интерес и уважение. То, что проверено веками и тысячелетиями, достойно внимательного изучения.</p>
-		</div>
-	</div>
-</div>
 <!--end text block-->
-
+	<div class="bottom-layout"></div>
+</div> <!-- end wrap-->
 <!--footer-->
 <footer>
 	<div class="container">
@@ -223,8 +224,8 @@
 			<div class="footer-menu">
 				<div class="col-xs-3">
 					<ul class="first-footer-menu">
-						<a href="#"><li>Главная</li></a>
-						<a href="#"><li>Каталог товаров</li></a>
+						<a href="index.php"><li>Главная</li></a>
+						<a href="catalog.html"><li>Каталог товаров</li></a>
 						<a href="#"><li>О льне</li></a>
 						<a href="#"><li>Доставка/Оплата</li></a>
 					</ul>
