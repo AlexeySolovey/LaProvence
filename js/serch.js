01fdb3768c6372ca159356c5f339371a0212548c
Alexey <Solovey_alexey3@mail.ru>
2016-03-19 18:29:05 +0200

    serch

---
+++ js/serch.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$(function () {
+		$('#serch').click(function(){
+			$('.serch-fild').toggle(300);
+			$('.register').toggle(300);
+			$('.enter').toggle(300);
+		});
+	});
+});
