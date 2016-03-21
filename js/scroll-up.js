a67b632a901e051c7f968fd80994541ba91b88ec
Alexey <Solovey_alexey3@mail.ru>
2016-03-21 13:25:46 +0200

    scroll-up

---
+++ js/scroll-up.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   $(function(){
+      $('.back-top').click(function(){
+        $('body,html').animate({
+            scrollTop:0}, 3000);
+        return false;
+      });
+   });
+          
+    $(function($){
+        $(document).on('click', 'a[href^=#]', function () {
+            $('html, body').animate({ scrollTop:  $('a[name="'+this.hash.slice(1)+'"]').offset().top }, 1000 ); 
+        return false;
+        });
+    });     
+});
